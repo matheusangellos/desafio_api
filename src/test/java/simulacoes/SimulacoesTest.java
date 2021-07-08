c3f6697a9deb487b04e1be9c1a79b79b01b060f2
Matheus Angellos <matheus_angellos@hotmail.com>
2021-07-08 10:59:01 -0300

    Adicionando testes de validação de Valor e Parcela.

--- src/test/java/simulacoes/SimulacoesTest.java
+++ src/test/java/simulacoes/SimulacoesTest.java
@@ -19,6 +19,50 @@ public class SimulacoesTest extends BaseAPI {
     Random gerador = new Random();
 
     @Test
+    public void validarRegraValor() {
+        String nome = feku.name().fullName();
+        String cpf = "0123456789" + gerador.nextInt(9);
+        String email = feku.name().firstName() + gerador.nextInt(100) + "@test.com";
+        int valor = gerador.nextInt(100) + 40000;
+        int parcelas = gerador.nextInt(46) + 2;
+        boolean seguro = gerador.nextBoolean();
+
+        Simulacao simulacao =
+                Simulacao.builder().nome(nome).cpf(cpf).email(email).valor(valor).parcelas(parcelas).seguro(seguro).build();
+
+        given().
+                contentType(ContentType.JSON).
+                body(simulacao).
+                when().
+                post("/v1/simulacoes").
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST).
+                body(containsString("Valor deve ser menor ou igual a R$ 40.000"));
+    }
+
+    @Test
+    public void validarRegraParcela() {
+        String nome = feku.name().fullName();
+        String cpf = "0123456789" + gerador.nextInt(9);
+        String email = feku.name().firstName() + gerador.nextInt(100) + "@test.com";
+        int valor = gerador.nextInt(39000) + 1000;
+        int parcelas = 0;
+        boolean seguro = gerador.nextBoolean();
+
+        Simulacao simulacao =
+                Simulacao.builder().nome(nome).cpf(cpf).email(email).valor(valor).parcelas(parcelas).seguro(seguro).build();
+
+        given().
+                contentType(ContentType.JSON).
+                body(simulacao).
+                when().
+                post("/v1/simulacoes").
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST).
+                body(containsString("Parcelas deve ser igual ou maior que 2"));
+    }
+
+    @Test
     public void retornarTodasSimulacoesComSucesso() {
         given().
                 when().
